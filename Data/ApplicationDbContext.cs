721d308fbe3a80b539b0b68206c47813d3d073f3
coderishaq103 <ishaqraja103@gmail.com>
2023-06-28 11:40:51 +0500

    comitting a full folder

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using aspcoreauth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace aspcoreauth.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Models.SalesLeadEntity> SalesLead { get; set; }
+    }
+}
